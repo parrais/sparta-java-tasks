2192f96a73cdd7cbed3059c9ce8ecfbbc16e930c
parrais <github@parrais.co.uk>
2025-10-14 10:08:02 +0100

    Complete 'Debugging Code' task

--- src/com/sparta/debug/DebugApp.java
+++ src/com/sparta/debug/DebugApp.java
@@ -1,4 +1,34 @@
 package com.sparta.debug;
 
 public class DebugApp {
+    public static void main(String[] args) {
+        String name = "Josh Crabtree";
+        String occupation = "trainee";
+        String location = "London";
+        String activityLevel = "moderate";
+
+        System.out.println("My name is " + name);
+        System.out.printf("I am a %s in %s%n", occupation, location);
+
+        int age = 21;
+
+        System.out.println("I am " + age + " years of age with a " + activityLevel + " activity level");
+    }
 }
+
+//public class DebugApp {
+//    public static void main(String[] args) {
+//        var name;
+//        name = "Josh Crabtree"
+//        occupation = "trainee";
+//        String location = 'London';
+//        var activity level = "moderate";
+//
+//        System.out.println("My name is", name);
+//        System.out.printf("I am a %s in %s%n" + location, occupation));
+//
+//        int age == 21;
+//
+//        print("I am " + age + "years of age with a ", activity level, "activity level");
+//    }
+//}
