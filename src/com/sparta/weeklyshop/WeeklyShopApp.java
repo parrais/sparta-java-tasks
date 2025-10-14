8feda9d2a47267c937dc35e33a39b7864ebd5ee5
parrais <github@parrais.co.uk>
2025-10-14 16:30:02 +0100

    Complete 'Shopping List' task

---
+++ src/com/sparta/weeklyshop/WeeklyShopApp.java
@@ -0,0 +1,33 @@
+package com.sparta.weeklyshop;
+
+import java.util.Scanner;
+
+public class WeeklyShopApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Peaches%n-how many?");
+        int peachesAmount = scanner.nextInt();
+        System.out.println("-price?");
+        double peachesPrice = scanner.nextDouble();
+        System.out.printf("Beans (cans of)%n-how many?");
+        int beansAmount = scanner.nextInt();
+        System.out.println("-price?");
+        double beansPrice = scanner.nextDouble();
+        System.out.printf("Chicken pieces (packets of)%n-how many?");
+        int chickenAmount = scanner.nextInt();
+        System.out.println("-price?");
+        double chickenPrice = scanner.nextDouble();
+        System.out.printf("Socks (pairs of)%n-how many?");
+        int socksAmount = scanner.nextInt();
+        System.out.println("-price?");
+        double socksPrice = scanner.nextDouble();
+        System.out.printf("Bottles of water%n-how many?");
+        int waterAmount = scanner.nextInt();
+        System.out.println("-price?");
+        double waterPrice = scanner.nextDouble();
+        int totalItems = peachesAmount + beansAmount + chickenAmount + socksAmount + waterAmount;
+        double totalPrice = (double) peachesAmount * peachesPrice + (double) beansAmount * beansPrice
+                + (double) chickenAmount * chickenPrice + (double) socksAmount * socksPrice + (double) waterAmount * waterPrice;
+        System.out.printf("Total number of items purchased: %d%nYour weekly shop cost: %3.2f", totalItems, totalPrice);
+    }
+}
