65cc68f00d9d1ee8acc3e217a746b6eec588d390
parrais <github@parrais.co.uk>
2025-10-14 16:11:27 +0100

    Refactor 'Cuboid' to remove unneeded lines

--- src/com/sparta/cuboid/CuboidApp.java
+++ src/com/sparta/cuboid/CuboidApp.java
@@ -7,13 +7,10 @@ public class CuboidApp {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("(All responses must be positive integers.)%nWhat is the width of your cuboid?");
         int width = scanner.nextInt();
-        scanner.nextLine();
         System.out.println("What is the length of your cuboid?");
         int length = scanner.nextInt();
-        scanner.nextLine();
         System.out.println("What is the height of your cuboid?");
         int height = scanner.nextInt();
-        scanner.nextLine();
         int surfaceArea = 2 * (width * length + width * height + length * height);
         int volume = width * length * height;
         System.out.printf("Your cuboid has surface area %d and volume %d", surfaceArea, volume);
