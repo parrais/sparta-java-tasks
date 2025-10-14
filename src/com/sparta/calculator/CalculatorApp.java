7d453cdccc7d54cf4aee19f3a9549ed61ef43383
parrais <github@parrais.co.uk>
2025-10-14 11:25:16 +0100

    Complete 'Simple Calculator' task

---
+++ src/com/sparta/calculator/CalculatorApp.java
@@ -0,0 +1,12 @@
+package com.sparta.calculator;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        int numerator = 15;
+        int denominator = 10;
+
+        float result = (float) numerator / (float) denominator;
+
+        System.out.println(result);
+    }
+}
