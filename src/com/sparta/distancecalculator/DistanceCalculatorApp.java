d4b523b1d535245e7a2d4ae3839bcd32ad8cb461
parrais <github@parrais.co.uk>
2025-10-14 17:01:51 +0100

    Complete 'Distance Calculator' task

---
+++ src/com/sparta/distancecalculator/DistanceCalculatorApp.java
@@ -0,0 +1,17 @@
+package com.sparta.distancecalculator;
+
+import java.util.Scanner;
+
+public class DistanceCalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Initial velocity (m/s)? ");
+        double initialVelocity = scanner.nextDouble();
+        System.out.print("time taken (s)? ");
+        double timeTaken = scanner.nextDouble();
+        System.out.print("acceleration (m/s^2)? ");
+        double acceleration = scanner.nextDouble();
+        double distance = initialVelocity * timeTaken + 0.5 * acceleration * timeTaken * timeTaken;
+        System.out.printf("The distance travelled is %3.2f metres.%n", distance);
+    }
+}
