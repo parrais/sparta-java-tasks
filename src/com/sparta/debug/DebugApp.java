1b0eb1c4c9406fdf159024b74c089fa565d7e77e
parrais <github@parrais.co.uk>
2025-10-14 12:41:06 +0100

    Minor improvements to solutions

--- src/com/sparta/debug/DebugApp.java
+++ src/com/sparta/debug/DebugApp.java
@@ -16,19 +16,23 @@ public class DebugApp {
     }
 }
 
-//public class DebugApp {
-//    public static void main(String[] args) {
-//        var name;
-//        name = "Josh Crabtree"
-//        occupation = "trainee";
-//        String location = 'London';
-//        var activity level = "moderate";
-//
-//        System.out.println("My name is", name);
-//        System.out.printf("I am a %s in %s%n" + location, occupation));
-//
-//        int age == 21;
-//
-//        print("I am " + age + "years of age with a ", activity level, "activity level");
-//    }
-//}
+/* Original bugged version:
+
+public class DebugApp {
+    public static void main(String[] args) {
+        var name;
+        name = "Josh Crabtree"
+        occupation = "trainee";
+        String location = 'London';
+        var activity level = "moderate";
+
+        System.out.println("My name is", name);
+        System.out.printf("I am a %s in %s%n" + location, occupation));
+
+        int age == 21;
+
+        print("I am " + age + "years of age with a ", activity level, "activity level";
+        }
+}
+
+*/
