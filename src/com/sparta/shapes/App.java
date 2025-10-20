a437046af1a40bb568004b527a3c4357f616b604
parrais <github@parrais.co.uk>
2025-10-20 12:29:09 +0100

    Add rectangle to 'Shapes' task

--- src/com/sparta/shapes/App.java
+++ src/com/sparta/shapes/App.java
@@ -2,10 +2,12 @@ package com.sparta.shapes;
 
 public class App {
     static void main() {
-        Circle c = new Circle(12.0);
-        Square sq = new Square(8.0);
+        Shape c = new Circle(12.0);
+        Shape sq = new Square(8.0);
+        Shape rect = new Rectangle(4.0, 5.0);
 
         System.out.println(c);      // outputs This shape has an area of 452.3893421169302
         System.out.println(sq);     // outputs This shape has an area of 64.0
+        System.out.println(rect);
     }
 }
